47da68c711a913888ce95d12c29efe8c9c3a3e07
infuq <infuq217@outlook.com>
2022-08-28 05:41:42 +0800

    ...

--- example/src/main/java/com/infuq/ServerOutHandler.java
+++ example/src/main/java/com/infuq/ServerOutHandler.java
@@ -11,41 +11,47 @@ public class ServerOutHandler implements ChannelOutboundHandler {
     @Override
     public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
         System.out.println("ServerOutHandler#bind");
+        ctx.bind(localAddress, promise);
     }
 
     @Override
     public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
         System.out.println("ServerOutHandler#connect");
+        ctx.connect(remoteAddress, localAddress, promise);
     }
 
     @Override
     public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
         System.out.println("ServerOutHandler#disconnect");
+        ctx.disconnect(promise);
     }
 
     @Override
     public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
         System.out.println("ServerOutHandler#close");
+        ctx.close(promise);
     }
 
     @Override
     public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
         System.out.println("ServerOutHandler#deregister");
+        ctx.deregister(promise);
     }
 
     @Override
     public void read(ChannelHandlerContext ctx) throws Exception {
-
+        System.out.println("ServerOutHandler#read");
+        ctx.read();
     }
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-
+        ctx.write(msg, promise);
     }
 
     @Override
     public void flush(ChannelHandlerContext ctx) throws Exception {
-
+        ctx.flush();
     }
 
 
@@ -62,6 +68,7 @@ public class ServerOutHandler implements ChannelOutboundHandler {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         System.out.println("ServerOutHandler#exceptionCaught");
+        ctx.fireExceptionCaught(cause);
     }
 
 
